fb8c0e1b719c2231451df6941012ce7a72996774
G6EJD <d.l.bird@sky.com>
2025-01-01 16:31:53 +0000

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,4 @@
+char ssid[]            = "yourSSID";                // your network SSID
+char password[]        = "yourPASSWORD";            // your network PASSWORD
+String API_Key         = "yourHomeAssistantAPIKey"; // From HA See readme
+String HomeAssistantIP = "192.168.0.1"              // your Home Asssistant IP Address (example address here, use yours)
